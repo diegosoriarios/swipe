2eaa757f742d086cd9087685e6b9478f4b5df687
diego.rios <diego.rios@CTNAPPLE01YG8WN.local>
2019-01-18 22:09:50 -0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Cards, { Card } from 'react-swipe-card'
 import './App.css';
+import './styles.css';
+import strings from './Strings';
+
+const CustomAlertLeft = () => <span>Nop</span>
+const CustomAlertRight = () => <span>Ok</span>
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      questions: [],
+      index: 0,
+      dit: 30,
+      riot: 30,
+    }
+  }
+
+  action(name) {
+    this.setState({
+      index: this.state.index + 1,
+    })
+    console.log(name);
+  }
+
+  componentDidMount = () => {
+    this.getQuestions()
+  }
+
+  getQuestions = () => {
+    let org = Math.floor(Math.random() * 2)
+    let question;
+    let test = []
+    for(var i = 0; i < 20; i++){
+      switch(org){
+        case 0:
+          question = Math.floor(Math.random() * strings.dit.length)
+          test = test.concat(strings.dit[question])
+          break;
+        case 1:
+          question = Math.floor(Math.random() * strings.riot.length)
+          test = test.concat(strings.riot[question])
+          break;
+        default:
+          console.log(strings)
+          break;
+      } 
+    }
+    this.setState({
+      questions: test
+    })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    if(this.state.questions.length > 0){
+      return (
+        <div className="b-content">
+            <ul className="status">
+              <li><div>D</div><div>{this.state.dit}</div></li>
+              <li><div>R</div><div>{this.state.riot}</div></li>
+            </ul>
+            <p>{this.state.questions[this.state.index].desc}</p>
+            <Cards 
+              onEnd={()=>{this.action('end')}} 
+              className='master-root'
+              alertRight={<CustomAlertRight />} 
+              alertLeft={<CustomAlertLeft />} 
+            >
+                {this.state.questions.map((item, i) =>
+                    <Card key={i}
+                        onSwipeLeft={()=>{this.action('swipe left')}}
+                        onSwipeRight={()=>{this.action('swipe right')}}>
+                        <img src={item.img} alt={item.name} style={{width: '100%', height: '100%'}}/>
+                    </Card>
+                )}
+            </Cards>
+        </div>
+      )
+    }else{
+      return <div>Loading...</div>
+    }
   }
 }
 
---
+++ src/Strings.js
@@ -0,0 +1,12 @@
+const strings = {
+    dit: [
+        {desc: '?', img: 'https://api.adorable.io/avatars/400/abott@adorable.io.png', status: [0, 0, 0, 1]},
+        {desc: '!', img: 'https://api.adorable.io/avatars/face/eyes4/nose3/mouth7/8e8895', status: [0, 0, 1, 0]},
+    ],
+    riot: [
+        {desc: '/', img: 'https://api.adorable.io/avatars/285/abtt%40adorable.io', status: [1, 0, 0, 0]},
+        {desc: '*', img: 'https://api.adorable.io/avatars/285/att%40adorable.io', status: [0, 1, 0, 0]},
+    ]
+}
+
+export default strings
